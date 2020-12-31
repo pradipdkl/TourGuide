b732d213047d97570125dc29a6d609dfa1321161
Pradip Dkl <pradipdkl46@gmail.com>
2020-12-31 12:22:52 +0545

    splash screen and OTP auth completed

---
+++ app/src/main/java/com/example/tourguide/SplashScreenActivity.java
@@ -0,0 +1,64 @@
+package com.example.tourguide;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    ImageView backgroundImage;
+
+    //animation
+    Animation sideAnim;
+    SharedPreferences onBoardingScreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        //binding
+        backgroundImage = findViewById(R.id.backgroundImage);
+
+        //Animation
+        sideAnim = AnimationUtils.loadAnimation(this, R.anim.side_anim);
+
+//        setAnimation
+
+        backgroundImage.setAnimation(sideAnim);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                onBoardingScreen = getSharedPreferences("onBoardingScreen", MODE_PRIVATE);
+                boolean isFirstTime = onBoardingScreen.getBoolean("FirstTime",true);
+
+                if (isFirstTime){
+
+                    SharedPreferences.Editor editor = onBoardingScreen.edit();
+                    editor.putBoolean("FirstTime", false);
+                    editor.commit();
+
+                    Intent intent  = new Intent(SplashScreenActivity.this, OnBoardingActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+                else {
+                    Intent intent  = new Intent(SplashScreenActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        },5000);
+
+
+    }
+}
